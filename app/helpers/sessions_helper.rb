755ac54679f21d3ef2205b92362c35f2cd96da3a
taichanNE30 <ne300209@senshu-u.jp>
2018-12-26 14:56:01 +0900

    Finish user edit, updare, index,adn destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-  #渡されたユーザーでログインする
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -10,6 +10,11 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 渡されたユーザーがログイン済みであればtrueを返す
+  def current_user?(user)
+    user == current_user
   end
 
   # 現在ログイン中のユーザーを返す(いる場合)
@@ -41,5 +46,16 @@ module SessionsHelper
     forget current_user
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
